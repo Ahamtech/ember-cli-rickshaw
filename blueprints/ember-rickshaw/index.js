ffaef35f1a8c05b39c73bdb539081d2293621a40
Anwesh <kanishkablack@gmx.com>
2016-08-26 22:58:36 +0530

    rolling back to rickshaw 1.5.10

--- blueprints/ember-rickshaw/index.js
+++ blueprints/ember-rickshaw/index.js
@@ -2,6 +2,6 @@ module.exports = {
   description: 'add rickshaw bower package',
   normalizeEntityName: function() {},
   afterInstall: function(options) {
-    return this.addBowerPackageToProject('rickshaw', '1.6.0-rc.0');
+    return this.addBowerPackageToProject('rickshaw', '1.5.10');
   }
 };
