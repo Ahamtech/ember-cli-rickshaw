584ac9605298384edcdede8b16307b625725aa38
Anwesh <kanishkablack@gmx.com>
2016-08-27 01:38:15 +0530

    typo udpates

--- addon/components/ember-rickshaw.js
+++ addon/components/ember-rickshaw.js
@@ -10,13 +10,12 @@ export default Ember.Component.extend({
       element: document.querySelector("#chart"), 
       width: this.get("width"), 
       height: this.get("height"),
-      renderer: this.get('renderer')
+      renderer: this.get('renderer'),
       series: [{
         color: 'steelblue',
         data: data
-      });
+      }]});
     graph.render();
-    var chart = new Chart(context)[type](data, options);
     this.set('graph', graph);
     this.addObserver('data', this, this.updateChart);
     this.addObserver('data.[]', this, this.updateChart);
