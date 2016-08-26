fa20ae1698068de188213d5813a3ae0e536c4977
Anwesh <kanishkablack@gmx.com>
2016-08-27 01:16:46 +0530

    initial rendering

--- addon/components/ember-rickshaw.js
+++ addon/components/ember-rickshaw.js
@@ -3,4 +3,25 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'div',
   
+  didInsertElement: function(){
+    var data = this.get('data');
+    var options = this.get('options');
+    var graph = new Rickshaw.Graph( {
+      element: document.querySelector("#chart"), 
+      width: this.get("width"), 
+      height: this.get("height"),
+      renderer: this.get('renderer')
+      series: [{
+        color: 'steelblue',
+        data: data
+      });
+    graph.render();
+    var chart = new Chart(context)[type](data, options);
+    this.set('graph', graph);
+    this.addObserver('data', this, this.updateChart);
+    this.addObserver('data.[]', this, this.updateChart);
+    this.addObserver('options', this, this.updateChart);
+  },
+  
+
 });
