ffd592d211e959153c67ff2606a8180b7a32acd9
Anwesh <kanishkablack@users.noreply.github.com>
2016-09-21 15:55:44 +0530

    rickshaw update

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ module.exports = {
   included: function included(app) {
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/d3/d3.min.js');
-    app.import(app.bowerDirectory + '/rickshaw/rickshaw.min.js');
-    app.import(app.bowerDirectory + '/rickshaw/rickshaw.min.css');
+    app.import(app.bowerDirectory + '/d3/d3.js');
+    app.import(app.bowerDirectory + '/rickshaw/rickshaw.js');
+    app.import(app.bowerDirectory + '/rickshaw/rickshaw.css');
   }
 };
