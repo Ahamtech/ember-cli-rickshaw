fc90adcc31e978e7cef14981f5f9e832e831a871
Anwesh <kanishkablack@users.noreply.github.com>
2016-10-04 06:58:54 +0530

    Update index.js

--- blueprints/ember-rickshaw/index.js
+++ blueprints/ember-rickshaw/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   description: 'add rickshaw bower package',
   normalizeEntityName: function() {},
-  afterInstall: function(options) {
+  beforeInstall: function(options) {
     return this.addBowerPackageToProject('rickshaw', '1.5.10');
   }
 };
